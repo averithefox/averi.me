747506eff7b283931b1feb1264589126dd2a6efb
Averi <git@averi.me>
2024-12-09 22:57:52 +0100

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="w-full h-full bg-vue-black font-['Indie_Flower'] text-vue-white">
+    <img alt="Hai :3" src="@/assets/hello.png" class="absolute bottom-0 right-0 w-[25rem] max-sm:hidden" />
+    <h1 class="text-5xl text-center pt-10">Welcome!</h1>
+    <h2 class="text-3xl text-center pt-5">Maybe something will appear here in the future</h2>
+  </main>
+</template>
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss"
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "vue": {
+          "black": {
+            "DEFAULT": "#181818",
+            "soft": "#222222",
+            "mute": "#282828"
+          },
+          "indigo": "#2c3e50",
+          "white": {
+            "DEFAULT": "#ffffff",
+            "soft": "#f8f8f8",
+            "mute": "#f2f2f2"
+          }
+        }
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
